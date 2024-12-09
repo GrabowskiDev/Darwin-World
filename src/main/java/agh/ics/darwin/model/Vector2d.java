e818da6a4daa5d49b985bd9606a9eecd15f26274
Łukasz Grabowski <lukasz.grabowski1@protonmail.com>
2024-12-09 19:48:06 +0100

    Add world and Vector2d

---
+++ src/main/java/agh/ics/darwin/model/Vector2d.java
@@ -0,0 +1,38 @@
+package agh.ics.darwin.model;
+
+import java.util.Objects;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Vector2d)) return false;
+        Vector2d other = (Vector2d) obj;
+        return x == other.x && y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
