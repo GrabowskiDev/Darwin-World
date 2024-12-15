c92bf67357c9a5762769315a53cd3ac0a2014ffc
Łukasz Grabowski <lukasz.grabowski1@protonmail.com>
2024-12-15 19:05:25 +0100

    Add basic animal

--- src/main/java/agh/ics/darwin/model/MapDirection.java
+++ src/main/java/agh/ics/darwin/model/MapDirection.java
@@ -36,24 +36,16 @@ public enum MapDirection {
         };
     }
 
-    public void rotate(int n) {
-        if (n>0) {
-            for (int i = 0; i < n; i++) {
-                this.next();
-            }
-        }
-    }
-
-    public MapDirection prev() {
+    public Vector2d toUnitVector() {
         return switch (this) {
-            case NORTH -> NORTHWEST;
-            case NORTHEAST -> NORTH;
-            case EAST -> NORTHEAST;
-            case SOUTHEAST -> EAST;
-            case SOUTH -> SOUTHEAST;
-            case SOUTHWEST -> SOUTH;
-            case WEST -> SOUTHWEST;
-            case NORTHWEST -> WEST;
+            case NORTH -> new Vector2d(0, 1);
+            case NORTHEAST -> new Vector2d(1, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTHEAST -> new Vector2d(1, -1);
+            case SOUTH -> new Vector2d(0, -1);
+            case SOUTHWEST -> new Vector2d(-1, -1);
+            case WEST -> new Vector2d(-1, 0);
+            case NORTHWEST -> new Vector2d(-1, 1);
         };
     }
 }
